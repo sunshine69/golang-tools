ea66e0426df90b7c5b672339720ab5104eb31570
Steve Kieu <msh.computing@gmail.com>
2021-09-01 16:23:29 +1000

    Add script to set ContainerExpirationPolicyAttributes

---
+++ gitlab/set-image-expiry-policy/go.mod
@@ -0,0 +1,23 @@
+module gitlab-set-image-expirey-policy
+
+go 1.17
+
+require (
+	github.com/sunshine69/golang-tools/utils v0.0.0-20210826005822-884adaf01b42
+	github.com/xanzy/go-gitlab v0.50.3
+)
+
+require (
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.1 // indirect
+	github.com/hashicorp/go-retryablehttp v0.6.8 // indirect
+	github.com/hashicorp/logutils v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/oauth2 v0.0.0-20181106182150-f42d05182288 // indirect
+	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
+	google.golang.org/appengine v1.3.0 // indirect
+)
---
+++ gitlab/set-image-expiry-policy/set-image-exp-policy.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"github.com/xanzy/go-gitlab"
+	u "github.com/sunshine69/golang-tools/utils"
+)
+
+var (
+	GitLabToken string
+)
+func main() {
+	GitLabToken = os.Getenv("GITLAB_TOKEN")
+	git, err := gitlab.NewClient(GitLabToken, gitlab.WithBaseURL("https://code.go1.com.au/api/v4"))
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+	opt := &gitlab.ListProjectsOptions{
+		Search: gitlab.String("DevOps"),
+		ListOptions: gitlab.ListOptions{
+			PerPage: 10,
+			Page:    1,
+		},
+	}
+	Cadence := "1month"
+	Enabled := true
+	NameRegexDelete := ".*"
+	NameRegexKeep := ".*(main|master)$"
+	KeepN := 50
+	OlderThan := "90d"
+
+	containerExpirationPolicyAttributes := gitlab.ContainerExpirationPolicyAttributes{
+		Cadence: &Cadence,
+		Enabled: &Enabled,
+		NameRegexDelete: &NameRegexDelete,
+		NameRegexKeep: &NameRegexKeep,
+		KeepN: &KeepN,
+		OlderThan: &OlderThan,
+	}
+	editPrjOpt := gitlab.EditProjectOptions{
+		ContainerExpirationPolicyAttributes: &containerExpirationPolicyAttributes,
+	}
+
+	for{
+		projectService := git.Projects
+		projects, resp, err := projectService.ListProjects(opt)
+		u.CheckErr(err, "Projects.ListProjects")
+
+		projectIDList := []int{}
+		for _, row := range projects {
+			projectIDList = append(projectIDList, row.ID )
+			_, _, err := projectService.EditProject(row.ID, &editPrjOpt)
+			u.CheckErr(err, "projectService.EditProject")
+		}
+		fmt.Printf("%v\n", projectIDList)
+
+
+		// Exit the loop when we've seen all pages.
+		if resp.CurrentPage >= resp.TotalPages {
+			break
+		}
+		// Update the page number to get the next page.
+		opt.Page = resp.NextPage
+	}
+}
