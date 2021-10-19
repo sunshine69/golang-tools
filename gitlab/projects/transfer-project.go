ff3a4f909989a0616aa87a5018126efb70a20c0c
Steve Kieu <msh.computing@gmail.com>
2021-10-19 17:45:50 +1000

    Remove the docker image for the temp one
    we forgot to do it

--- gitlab/projects/transfer-project.go
+++ gitlab/projects/transfer-project.go
@@ -1,72 +1,79 @@
 package main
 
 import (
-	"log"
 	"fmt"
-	u "localhost.com/utils"
-	. "localhost.com/gitlab/model"
+	"log"
+
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/xanzy/go-gitlab"
+	. "localhost.com/gitlab/model"
+	u "localhost.com/utils"
 )
-func MoveProjectRegistryImages(git *gitlab.Client, currentPrj, tempPrj *gitlab.Project ) {
+
+func MoveProjectRegistryImages(git *gitlab.Client, currentPrj, tempPrj *gitlab.Project) {
 	returnTag := true
 	registryRepos, _, err := git.ContainerRegistry.ListRegistryRepositories(currentPrj.ID, &gitlab.ListRegistryRepositoriesOptions{
 		ListOptions: gitlab.ListOptions{
 			Page: 1, PerPage: 500,
 		},
-		Tags: &returnTag,
+		Tags:      &returnTag,
 		TagsCount: &returnTag,
-	}); u.CheckErr(err, "MoveProjectRegistryImages ListRegistryRepositories")
+	})
+	u.CheckErr(err, "MoveProjectRegistryImages ListRegistryRepositories")
 
 	oldImagesList := []string{}
 	for _, repoReg := range registryRepos {
 		_, tags := repoReg.Location, repoReg.Tags
-		for _, t := range tags{
+		for _, t := range tags {
 			oldImage := t.Location
 			oldImagesList = append(oldImagesList, oldImage)
-			extraName := u.Ternary( repoReg.Name == "", "", "/" + repoReg.Name )
-			newImage := fmt.Sprintf(`%s%s:%s`, GetContainerRegistryBaseLocation(git, tempPrj.ID), extraName, t.Name )
-			u.RunSystemCommand( fmt.Sprintf("docker pull %s", oldImage) , true)
-			u.RunSystemCommand(fmt.Sprintf("docker tag %s  %s", oldImage, newImage ), true)
-			u.RunSystemCommand( fmt.Sprintf("docker push %s", newImage), true )
+			extraName := u.Ternary(repoReg.Name == "", "", "/"+repoReg.Name)
+			newImage := fmt.Sprintf(`%s%s:%s`, GetContainerRegistryBaseLocation(git, tempPrj.ID), extraName, t.Name)
+			u.RunSystemCommand(fmt.Sprintf("docker pull %s", oldImage), true)
+			u.RunSystemCommand(fmt.Sprintf("docker tag %s  %s", oldImage, newImage), true)
+			u.RunSystemCommand(fmt.Sprintf("docker push %s", newImage), true)
 		}
 	}
 	for _, repoReg := range registryRepos {
 		_, err := git.ContainerRegistry.DeleteRegistryRepository(currentPrj.ID, repoReg.ID, nil)
-		u.CheckErr(err, "MoveProjectRegistryImages DeleteRegistryRepository " + repoReg.String())
+		u.CheckErr(err, "MoveProjectRegistryImages DeleteRegistryRepository "+repoReg.String())
 	}
 	// Clean up local docker images after pushing
-	for _, _oldImage := range oldImagesList{
+	for _, _oldImage := range oldImagesList {
 		u.RunSystemCommand(fmt.Sprintf(`docker rmi %s`, _oldImage), false)
 	}
 }
-func MoveProjectRegistryImagesUseShell(git *gitlab.Client, currentPrj, tempPrj *gitlab.Project, user string ) {
+func MoveProjectRegistryImagesUseShell(git *gitlab.Client, currentPrj, tempPrj *gitlab.Project, user string) {
 	returnTag := true
 	registryRepos, _, err := git.ContainerRegistry.ListRegistryRepositories(currentPrj.ID, &gitlab.ListRegistryRepositoriesOptions{
 		ListOptions: gitlab.ListOptions{
 			Page: 1, PerPage: 500,
 		},
-		Tags: &returnTag,
+		Tags:      &returnTag,
 		TagsCount: &returnTag,
-	}); u.CheckErr(err, "MoveProjectRegistryImages ListRegistryRepositories")
+	})
+	u.CheckErr(err, "MoveProjectRegistryImages ListRegistryRepositories")
 	for _, repoReg := range registryRepos {
 		repoImage := repoReg.Location
-		extraName := u.Ternary( repoReg.Name == "", "", "/" + repoReg.Name )
+		extraName := u.Ternary(repoReg.Name == "", "", "/"+repoReg.Name)
 		newImage := fmt.Sprintf(`%s%s`, GetContainerRegistryBaseLocation(git, tempPrj.ID), extraName)
 		u.RunSystemCommand(fmt.Sprintf("docker pull %s -a", repoImage), true)
 		log.Printf("Pull %s completed\nStart to push ...\n", repoImage)
 
-		u.SendMailSendGrid("Go1 GitlabDomain Automation <steve.kieu@go1.com>", user, fmt.Sprintf("Gitlab migration progress. Project %s", currentPrj.NameWithNamespace), fmt.Sprintf("We are going to push images From %s => %s", currentPrj.NameWithNamespace, tempPrj.NameWithNamespace), "", []string{} )
+		u.SendMailSendGrid("Go1 GitlabDomain Automation <steve.kieu@go1.com>", user, fmt.Sprintf("Gitlab migration progress. Project %s", currentPrj.NameWithNamespace), fmt.Sprintf("We are going to push images From %s => %s", currentPrj.NameWithNamespace, tempPrj.NameWithNamespace), "", []string{})
 
-		u.RunSystemCommand(fmt.Sprintf(`docker images %s --format "docker tag {{.Repository}}:{{.Tag}} %s:{{.Tag}} && docker push %s:{{.Tag}}" | bash `, repoImage, newImage, newImage ), true)
+		u.RunSystemCommand(fmt.Sprintf(`docker images %s --format "docker tag {{.Repository}}:{{.Tag}} %s:{{.Tag}} && docker push %s:{{.Tag}}" | bash `, repoImage, newImage, newImage), true)
 		log.Printf("Push %s completed\nStart to clean up ...\n", newImage)
 
-		u.SendMailSendGrid("Go1 GitlabDomain Automation <steve.kieu@go1.com>", user, fmt.Sprintf("Gitlab migration progress. Project %s", currentPrj.NameWithNamespace), "", fmt.Sprintf("We are going to delete the container registry repository of the project name %s, ID %d. <b>This means your production k8s if using the old image will get errors. To minimize the outage of scaling please keep an eye for next email for action</b>", currentPrj.NameWithNamespace, currentPrj.ID), []string{} )
+		u.SendMailSendGrid("Go1 GitlabDomain Automation <steve.kieu@go1.com>", user, fmt.Sprintf("Gitlab migration progress. Project %s", currentPrj.NameWithNamespace), "", fmt.Sprintf("We are going to delete the container registry repository of the project name %s, ID %d. <b>This means your production k8s if using the old image will get errors. To minimize the outage of scaling please keep an eye for next email for action</b>", currentPrj.NameWithNamespace, currentPrj.ID), []string{})
 
 		_, err := git.ContainerRegistry.DeleteRegistryRepository(currentPrj.ID, repoReg.ID, nil)
-		u.CheckErr(err, "MoveProjectRegistryImages DeleteRegistryRepository " + repoReg.String())
+		u.CheckErr(err, "MoveProjectRegistryImages DeleteRegistryRepository "+repoReg.String())
 
-		go u.RunSystemCommand(fmt.Sprintf(`docker images %s --format "docker rmi {{.Repository}}:{{.Tag}}" | bash`, repoImage), true)
+		go u.RunSystemCommand(fmt.Sprintf(
+			`docker images %s --format "docker rmi {{.Repository}}:{{.Tag}}" | bash
+			docker images %s --format "docker rmi {{.Repository}}:{{.Tag}}" | bash`,
+			repoImage, newImage), true)
 		log.Printf("Cleanup %s completed\n", repoImage)
 	}
 }
@@ -74,18 +81,20 @@ func BackupProjectRegistryImages(git *gitlab.Client, p *gitlab.Project, user str
 	tempPrjPath := fmt.Sprintf("%s-temp", p.Path)
 	newNameSpaceId := ProjectDomainGet(map[string]string{"where": fmt.Sprintf("project_id = %d", p.ID)})[0].DomainId
 	tempPrj, _, err := git.Projects.CreateProject(&gitlab.CreateProjectOptions{
-		Path: &tempPrjPath,
+		Path:        &tempPrjPath,
 		NamespaceID: &newNameSpaceId,
 	})
 	if u.CheckNonErrIfMatch(err, "has already been taken", "BackupProjectRegistryImages") != nil {
 		ps, _, err := git.Projects.ListProjects(&gitlab.ListProjectsOptions{
 			Search: &tempPrjPath,
-		}); u.CheckErr(err, "BackupProjectRegistryImages ListProjects")
+		})
+		u.CheckErr(err, "BackupProjectRegistryImages ListProjects")
 		tempPrj = ps[0]
 	}
 	MoveProjectRegistryImagesUseShell(git, p, tempPrj, user)
 	return tempPrj
 }
+
 // Take a project_id and automate the transfer process. This should be run on a dashboard manually per project
 // - Assign to new domain/team using gitlab API
 // - Trigger a build of the new project with the last release state
@@ -120,36 +129,37 @@ func TransferProject(git *gitlab.Client, gitlabProjectId int, user string) {
 	lastNewGroup := gitlabDomainGroup
 	nonCopyableVars := []*gitlab.GroupVariable{}
 	//Replicate group path from old project => new one
-	for idx, eg := range existingGroupList{
+	for idx, eg := range existingGroupList {
 		if idx == 0 {
 			log.Println("Copy the group vars - existingRootGroup => New Root Group")
-			nonCopyableVars = append(nonCopyableVars, CopyGroupVars(git, eg, gitlabDomainGroup)... )
+			nonCopyableVars = append(nonCopyableVars, CopyGroupVars(git, eg, gitlabDomainGroup)...)
 		} else {
 			log.Printf("Check if sub group exists in the new tree")
-			gs := GitlabNamespaceGet(map[string]string{"where":fmt.Sprintf("parent_id = %d AND path = '%s' ", parentID, eg.Path)})
+			gs := GitlabNamespaceGet(map[string]string{"where": fmt.Sprintf("parent_id = %d AND path = '%s' ", parentID, eg.Path)})
 
 			if len(gs) == 0 {
 				log.Printf("Group does not exist, creating new group with parentID %d\n", parentID)
 				lastNewGroup, _, err = git.Groups.CreateGroup(&gitlab.CreateGroupOptions{
 					ParentID: &parentID,
-					Path: &eg.Path,
-					Name: &eg.Path, //Simplify, maybe search replace - with space and Captialize word?
+					Path:     &eg.Path,
+					Name:     &eg.Path, //Simplify, maybe search replace - with space and Captialize word?
 				})
 				if u.CheckNonErrIfMatch(err, "has already been taken", "") != nil {
-					_gs,_,err := git.Groups.ListGroups(&gitlab.ListGroupsOptions{
-						Search: &eg.Path,
+					_gs, _, err := git.Groups.ListGroups(&gitlab.ListGroupsOptions{
+						Search:     &eg.Path,
 						SkipGroups: []int{eg.ID},
-					}); u.CheckErr(err, "TransferProject CreateGroup")
+					})
+					u.CheckErr(err, "TransferProject CreateGroup")
 					lastNewGroup = _gs[0]
 				}
 				GitlabNamespaceNew(lastNewGroup.FullPath) //Update the table so re-run will detect that
 			} else {
 				log.Printf("Group exist, copy vars over")
-				lastNewGroup,_,err = git.Groups.GetGroup(gs[0].GitlabNamespaceId, nil)
+				lastNewGroup, _, err = git.Groups.GetGroup(gs[0].GitlabNamespaceId, nil)
 				u.CheckErr(err, "TransferProject GetGroup")
 			}
 			log.Printf("[DEBUG] lastNewGroup %s\n", u.JsonDump(lastNewGroup, "  "))
-			nonCopyableVars = append(nonCopyableVars,  CopyGroupVars(git, eg, lastNewGroup)... )
+			nonCopyableVars = append(nonCopyableVars, CopyGroupVars(git, eg, lastNewGroup)...)
 			parentID = lastNewGroup.ID
 		}
 	}
@@ -165,9 +175,10 @@ func TransferProject(git *gitlab.Client, gitlabProjectId int, user string) {
 			ListOptions: gitlab.ListOptions{
 				Page: 1, PerPage: 500,
 			},
-			Tags: &returnTag,
+			Tags:      &returnTag,
 			TagsCount: &returnTag,
-		}); u.CheckErr(err, "MoveProjectRegistryImages ListRegistryRepositories")
+		})
+		u.CheckErr(err, "MoveProjectRegistryImages ListRegistryRepositories")
 		if len(registryRepos) == 0 {
 			log.Printf("No repo, no tags")
 			break
@@ -182,7 +193,8 @@ func TransferProject(git *gitlab.Client, gitlabProjectId int, user string) {
 	if u.CheckErrNonFatal(err, "TransferProject TransferGroup") != nil {
 		log.Fatalf("[ERROR] gitlab response is %s\n", u.JsonDump(res, "  "))
 	}
-	project.DomainOwnershipConfirmed = 1; project.Update()
+	project.DomainOwnershipConfirmed = 1
+	project.Update()
 
 	log.Printf("[DEBUG] Copy nonCopyableVars into project\n")
 	nonCopyableVars1 := CopyGroupVarIntoProject(git, nonCopyableVars, gitlabProject)
@@ -190,7 +202,7 @@ func TransferProject(git *gitlab.Client, gitlabProjectId int, user string) {
 		log.Printf("[WARN] Can not copy these vars, it does exist but having different values\n%s\n", u.JsonDump(nonCopyableVars1, "  "))
 	}
 
-	u.SendMailSendGrid("Go1 GitlabDomain Automation <steve.kieu@go1.com>", user, fmt.Sprintf("Gitlab migration progress. Project %s", gitlabProject.NameWithNamespace), "", fmt.Sprintf("<h2>Migration %s completed</h2> However we still need to move the images back. <b>You can start to rebuild and deploy now</b>. If you do not want to rebuild and just want to re-deploy qa and prod, check the container registry to be sure the latest image tag has been copied over and you can run the deploy job manualy.", gitlabProject.NameWithNamespace ), []string{} )
+	u.SendMailSendGrid("Go1 GitlabDomain Automation <steve.kieu@go1.com>", user, fmt.Sprintf("Gitlab migration progress. Project %s", gitlabProject.NameWithNamespace), "", fmt.Sprintf("<h2>Migration %s completed</h2> However we still need to move the images back. <b>You can start to rebuild and deploy now</b>. If you do not want to rebuild and just want to re-deploy qa and prod, check the container registry to be sure the latest image tag has been copied over and you can run the deploy job manualy.", gitlabProject.NameWithNamespace), []string{})
 
 	log.Println("Move container image from temp")
 	MoveProjectRegistryImagesUseShell(git, tempPrj, gitlabProject, user)
