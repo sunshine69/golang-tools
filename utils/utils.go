7c79ad759b62121b5474dfa6d390def874ac58bc
Steve Kieu <msh.computing@gmail.com>
2021-11-11 14:35:42 +1000

    Add Unzip function

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"archive/zip"
 	"html/template"
 	"path/filepath"
 	"errors"
@@ -51,6 +52,46 @@ const (
 var (
 	json = jsoniter.ConfigCompatibleWithStandardLibrary
 )
+
+func Unzip(zipfile, dst string) error {
+    archive, err := zip.OpenReader(zipfile)
+    if err != nil {
+        return err
+    }
+    defer archive.Close()
+    for _, f := range archive.File {
+        filePath := filepath.Join(dst, f.Name)
+        fmt.Println("unzipping file ", filePath)
+
+        if !strings.HasPrefix(filePath, filepath.Clean(dst)+string(os.PathSeparator)) {
+            fmt.Println("invalid file path")
+            return nil
+        }
+        if f.FileInfo().IsDir() {
+            fmt.Println("creating directory...")
+            os.MkdirAll(filePath, os.ModePerm)
+            continue
+        }
+        if err := os.MkdirAll(filepath.Dir(filePath), os.ModePerm); err != nil {
+            return err
+        }
+        dstFile, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+        if err != nil {
+            return err
+        }
+        fileInArchive, err := f.Open()
+        if err != nil {
+            return err
+        }
+        if _, err := io.Copy(dstFile, fileInArchive); err != nil {
+            return err
+        }
+        dstFile.Close()
+        fileInArchive.Close()
+    }
+	return nil
+}
+
 func FindAndParseTemplates(rootDir, fileExtention string,funcMap template.FuncMap) (*template.Template, []string, error) {
     cleanRoot := filepath.Clean(rootDir)
 	if fileExtention == "" { fileExtention = ".html" }
