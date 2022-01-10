b36c724c28bbd6ee24a9c332fc6a890084bd8276
Steve Kieu <msh.computing@gmail.com>
2022-01-11 09:55:16 +1000

    Delete review resources, not deleting service with external name

--- k8s-delete-old-deployment/k8s-delete-old-deployment.go
+++ k8s-delete-old-deployment/k8s-delete-old-deployment.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"regexp"
 	"os"
 	"flag"
 	"log"
@@ -19,7 +20,7 @@ var (
     autoExecute bool
 )
 
-func processCommand(objType, inputFile string) {
+func processCommand(objType, inputFile, namePattern string) {
 	data, err := ioutil.ReadFile(inputFile)
 	u.CheckErr(err, "")
 
@@ -27,12 +28,28 @@ func processCommand(objType, inputFile string) {
 	u.CheckErr(json.Unmarshal(data, &o), "")
 	items := o["items"].([]interface{})
 	output := []map[string]string{}
+
+	var filterNameRegex *regexp.Regexp = nil
+	if namePattern != "" {
+		filterNameRegex = regexp.MustCompile(namePattern)
+	}
 	for _, val := range items {
 		_val := val.(map[string]interface{})["metadata"].(map[string]interface{})
-		output = append(output, map[string]string{
-			"name":      _val["name"].(string),
-			"startdate": _val["creationTimestamp"].(string),
-		})
+		_spec := val.(map[string]interface{})["spec"].(map[string]interface{})
+		objName := _val["name"].(string)
+		doAppendToList := true
+		if filterNameRegex != nil {
+			doAppendToList = u.Ternary( filterNameRegex.MatchString(objName), true, false  ).(bool)
+		}
+		doAppendToList = u.Ternary(
+			(objType == "service") && (_spec["type"].(string) == "ExternalName"), false, doAppendToList	).(bool)
+
+		if doAppendToList {
+			output = append(output, map[string]string{
+				"name":      _val["name"].(string),
+				"startdate": _val["creationTimestamp"].(string),
+			})
+		}
 	}
 	cmdList := []string{}
 	for _, val := range output {
@@ -66,20 +83,26 @@ func main() {
 	supportObjectType := map[string]bool{
 		"deployment": true, "service": true, "svc": true, "ingress": true, "ing": true, "hpa": true, "all": true,
 	}
+	nameRegexLookup := map[string]string{
+		"deployment": ``,
+		"service": ``,
+		"ingress": ``,
+		"hpa": ``,
+	}
 	if isSupport, ok := supportObjectType[objectType]; ok {
 		if isSupport {
 			if autoExecute {
 				if objectType == "all" {
-					for _, objType := range []string{"deployment","service","ingress", "hpa"} {
+					for _, objType := range []string{"deployment", "service", "ingress", "hpa"} {
 						u.RunSystemCommand(fmt.Sprintf("kubectl -n %s get %s -o json > %s", k8sNamespace, objType, tempFile.Name()), false)
-						processCommand(objType, tempFile.Name())
+						processCommand(objType, tempFile.Name(), nameRegexLookup[objType] )
 					}
 				} else {
 					u.RunSystemCommand(fmt.Sprintf("kubectl -n %s get %s -o json > %s", k8sNamespace, objectType,tempFile.Name()), false)
-					processCommand(objectType, tempFile.Name())
+					processCommand(objectType, tempFile.Name(), nameRegexLookup[objectType])
 				}
 			} else {
-				processCommand(objectType, inputFile)
+				processCommand(objectType, inputFile, nameRegexLookup[objectType])
 			}
 		} else {
 			log.Fatalf("[ERROR] Whether the objectType (-t) is not supported")
