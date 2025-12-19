4d1a665e9b3b2f8b241e919d4e6acadc7b0bffd8
steve kieu <steve.kieu@sonichealthcare.com.au>
2025-12-19 14:41:23 +1000

    Maplookup use generic now

--- utils/utils.go
+++ utils/utils.go
@@ -1677,7 +1677,7 @@ func ReadPropertiesFile(filename string) (AppConfigProperties, error) {
 }
 
 // MapLookup search a key in a map and return the value if found, otherwise return the default_val
-func MapLookup(m map[string]any, key string, default_val any) any {
+func MapLookup[T any](m map[string]T, key string, default_val T) T {
 	if v, ok := m[key]; ok {
 		return v
 	} else {
